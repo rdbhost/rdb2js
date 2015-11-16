3cbde7e897f6e15bf35d153f53e163a41be2a45a
David <dkeeney@travelbyroad.net>
2015-11-15 18:45:32 -0700

    Initial vendor lib stocking

---
+++ test/private.js
@@ -0,0 +1,26 @@
+
+
+/**
+ * Created by JetBrains WebStorm.
+ * User: David
+ * Date: 4/15/12
+ * Time: 6:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+/*
+var domain = 'dev.rdbhost.com',
+    acct_number = 9,
+    demo_r_role = 'r0000000009',
+    demo_p_role = 'p0000000009',
+    demo_s_role = 's0000000009',
+    demo_email = 'dkeeney@travelbyroad.net';
+*/
+
+var domain = 'dev.rdbhost.com',
+    acct_number = 12,
+    demo_r_role = 'r0000000012',
+    demo_p_role = 'p0000000012',
+    demo_s_role = 's0000000012',
+    demo_email = 'js@travelbyroad.net';
