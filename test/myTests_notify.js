8e489cae562720edd72283a4daacbd22e9d1620b
David <dkeeney@travelbyroad.net>
2016-01-03 10:13:08 -0700

    livereload renamed to rdbhost_livereload test added for image reloading

--- test/myTests_notify.js
+++ test/myTests_notify.js
@@ -83,9 +83,20 @@ asyncTest('listen request receives ok', 7, function() {
 // send reader listen request with query, verify promise fulfilled
 //    and that notify is independently received
 //
-asyncTest('listen request receives ftp-channel message ok', 7, function() {
+asyncTest('listen request invokes reloader on image', 8, function() {
 
     var notifyrecd = false;
+
+    // add a (nonexistant) image tag to page
+    var el = document.createElement('img'),
+        src = document.createAttribute('src'),
+        id = document.createAttribute('id');
+    src.value = '/dummy.gif';
+    id.value = 'test-image';
+    el.setAttributeNode(src);
+    el.setAttributeNode(id);
+    document.body.appendChild(el);
+    var savedImgSrc = el.src;
 
     Rdbhost.once('notify-received:rdbhost_ftp_channel', function f(ch, pl) {
         ok(fail, 'notify-received should not be received');
@@ -93,13 +104,25 @@ asyncTest('listen request receives ftp-channel message ok', 7, function() {
     Rdbhost.once('reload-request:rdbhost_ftp_channel', function f(ch, pl) {
         ok('event', 'notify event received');
         ok(ch === 'rdbhost_ftp_channel', 'channel is correct');
-        ok(pl.substr(0,6) === 'test m', 'payload is correct');
+        ok(pl.substr(0,6) === 'SAVE F', 'payload is correct');
         notifyrecd = true;
+        setTimeout(function() {
+            var el1 = document.getElementById('test-image');
+            ok(el1.src !== savedImgSrc, 'src '+el1.src);
+        }, 10);
     });
 
     var r = Rdbhost.reader()
-        .query("NOTIFY \"rdbhost_ftp_channel\", 'test message on channel abc';")
+        .query("NOTIFY \"rdbhost_ftp_channel\", 'SAVE FILE /dummy.gif';")
         .listen('rdbhost_ftp_channel');
+
+    function cleanup() {
+        setTimeout(function() {
+            clearTimeout(st);
+            document.body.removeChild(el);
+            start();
+        }, 50)
+    }
 
     var p = r.go();
     ok(p.constructor.name === 'lib$es6$promise$promise$$Promise', p);
@@ -108,14 +131,12 @@ asyncTest('listen request receives ftp-channel message ok', 7, function() {
             ok(d.result_sets.length == 1, 'result_sets len');
             ok(d.result_sets[0].row_count[0] == -1, 'row_count === 1');
             if ( notifyrecd ) {
-                clearTimeout(st);
-                start();
+                cleanup();
             }
         })
         .catch(function(e) {
             ok(false, 'then error called');
-            clearTimeout(st);
-            start();
+            cleanup();
         });
 
     var st = setTimeout(function() { start(); }, 1000);
