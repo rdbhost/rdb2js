e1a53151e54e9666a5cf6176b74f183800f374ac
David <dkeeney@travelbyroad.net>
2016-01-30 20:44:47 -0700

    added dialogstress pages

---
+++ test/observe/private.js
@@ -0,0 +1,18 @@
+
+
+/**
+ * Created by JetBrains WebStorm.
+ * User: David
+ * Date: 4/15/12
+ * Time: 6:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+
+var bad_acct_number = 2,
+    bad_email = 'demo@travelbyroad.net';
+
+
+var domain = 'dev.rdbhost.com',
+    acct_number = 14,
+    demo_email = 'jsdemos@travelbyroad.net';
